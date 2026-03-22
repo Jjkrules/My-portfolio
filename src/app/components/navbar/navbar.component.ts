8b144de4a15bbeb9037452b10395b92635f5a5be
Jimmy Ren <jimmyren@Jimmys-MacBook-Pro.local>
2026-03-22 18:25:04 -0400

    added navbar style

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LucideAngularModule, User, FolderKanban, Briefcase, BadgeCheck } from 'lucide-angular';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [LucideAngularModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: '../../../styles.css'
+})
+export class NavbarComponent {
+  icons = { User, FolderKanban, Briefcase, BadgeCheck };
+}
