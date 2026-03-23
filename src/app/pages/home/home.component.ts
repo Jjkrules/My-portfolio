acdcb05828f36f55a4a9fb00490d08c74e6642e0
Jimmy Ren <jimmyren@Jimmys-MacBook-Pro.local>
2026-03-22 20:03:56 -0400

    temp landing page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { HeaderComponent } from "../../components/header/header.component";
+import { LandingComponent } from "../../components/landing/landing.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, HeaderComponent],
+  imports: [NavbarComponent, HeaderComponent, LandingComponent],
   templateUrl: './home.component.html',
   styleUrl: '../../../styles.css'
 })
