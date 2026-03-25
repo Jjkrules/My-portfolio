376c259607b3bce87046de986cde2ac86b444b7f
Jimmy Ren <jimmyren@Jimmys-MacBook-Pro.local>
2026-03-25 11:23:39 -0400

    added ability to focus and unfocus on boxes

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Renderer2, ViewChild, viewChild } from '@angular/core';
+import { Scale } from 'lucide-angular';
 
 @Component({
   selector: 'app-about-me',
@@ -25,16 +26,27 @@ export class AboutMeComponent {
       const isTarget = item.id === section;
 
       if (!isTarget && item.el.dataset['state'] === 'small') {return}
-      if(isTarget && item.el.dataset['state'] === 'large') {return}
+      if(isTarget && item.el.dataset['state'] === 'large') {
+        item.el.animate([
+          { transform: 'scale(1.1)', opacity: 1 },
+          { transform: 'scale(1)', opacity: 0.05 }
+        ], {
+          duration: 1000,
+          easing: 'ease-out',
+          fill: 'forwards'
+        })
+        item.el.dataset['state'] = 'small';
+        return;
+      }
       
       item.el.animate([
         { 
           transform: isTarget ? 'scale(1)' : 'scale(1.1)', 
-          opacity: isTarget ? 0.25 : 1 
+          opacity: isTarget ? 0.05 : 1 
         },
         { 
           transform: isTarget ? 'scale(1.1)' : 'scale(1)', 
-          opacity: isTarget ? 1 : 0.15 
+          opacity: isTarget ? 1 : 0.05 
         }
       ], {
         duration: 1000,
