d220c1f60054c25f175ca51ab33fa01e6540620b
Jr3582 <jr3582@drexel.edu>
2026-03-25 20:21:49 -0400

    about me btns now phases with the box

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, Renderer2, ViewChild, viewChild } from '@angular/core';
-import { Scale } from 'lucide-angular';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
@@ -11,15 +10,20 @@ export class AboutMeComponent {
   @ViewChild('sparkBox') sparkBox! : ElementRef;
   @ViewChild('futureBox') futureBox! : ElementRef;
 
+  @ViewChild('journeyBtn') journeyBtn! : ElementRef;
+  @ViewChild('sparkBtn') sparkBtn! : ElementRef;
+  @ViewChild('futureBtn') futureBtn! : ElementRef;
+
+
   activeSection: string = 'none';
 
   focusSection(section: string) {
     this.activeSection = section;
 
     const sections = [
-      { id: 'journey', el: this.journeyBox.nativeElement },
-      { id: 'spark', el: this.sparkBox.nativeElement },
-      { id: 'future', el: this.futureBox.nativeElement }
+      { id: 'journey', el: this.journeyBox.nativeElement, btn: this.journeyBtn.nativeElement },
+      { id: 'spark', el: this.sparkBox.nativeElement, btn: this.sparkBtn.nativeElement },
+      { id: 'future', el: this.futureBox.nativeElement, btn: this.futureBtn.nativeElement }
     ];
 
     sections.forEach(item => {
@@ -29,30 +33,56 @@ export class AboutMeComponent {
       if(isTarget && item.el.dataset['state'] === 'large') {
         item.el.animate([
           { transform: 'scale(1.1)', opacity: 1 },
-          { transform: 'scale(1)', opacity: 0.05 }
+          { transform: 'scale(1)', opacity: 0.15 }
+        ], {
+          duration: 1000,
+          easing: 'ease-out',
+          fill: 'forwards'
+        })
+        item.btn.animate([
+          { transform: 'scale(1.1)', opacity: 1 },
+          { transform: 'scale(1)', opacity: 0.15 }
         ], {
           duration: 1000,
           easing: 'ease-out',
           fill: 'forwards'
         })
         item.el.dataset['state'] = 'small';
+        item.btn.dataset['state'] = 'small';
         return;
       }
       
       item.el.animate([
         { 
-          transform: isTarget ? 'scale(1)' : 'scale(1.1)', 
-          opacity: isTarget ? 0.05 : 1 
+          transform: isTarget ? 'scale(1)' : 'scale(1.05)', 
+          opacity: isTarget ? 0.15 : 1 
         },
         { 
-          transform: isTarget ? 'scale(1.1)' : 'scale(1)', 
-          opacity: isTarget ? 1 : 0.05 
+          transform: isTarget ? 'scale(1.05)' : 'scale(1)', 
+          opacity: isTarget ? 1 : 0.15 
         }
       ], {
         duration: 1000,
         easing: 'ease-out',
         fill: 'forwards'
       });
+
+      item.btn.animate([
+        {
+          transform: isTarget ? 'scale(1)' : 'scale(1.05)',
+          opacity: isTarget ? 0.15 : 1
+        },
+        {
+          transform: isTarget ? 'scale(1.05)' : 'scale(1)',
+          opacity: isTarget ? 1 : 0.15
+        }
+      ], {
+        duration: 1000,
+        easing: 'ease-out',
+        fill: 'forwards'
+      })
+
+      item.btn.dataset['state'] = isTarget ? 'large' : 'small';
       item.el.dataset['state'] = isTarget ? 'large' : 'small';
     });
   }
