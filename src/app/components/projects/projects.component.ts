d0775cb0933ed2663fe70193e406e87fa4503922
Jimmy Ren <jimmyren@Jimmys-MacBook-Pro.local>
2026-03-28 16:09:02 -0400

    finished project section

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule, SquareArrowOutUpRight} from 'lucide-angular';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './projects.component.html',
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+  isBoxDrawn = false;
+
+  icons = { SquareArrowOutUpRight };
+
+  projects = [
+    {index: 0, title: "My Portfolio!", img:"projImg.png" , git_hub: "https://github.com/Jjkrules/My-portfolio", proj_link: "", description: "description text", isVisible: false},
+    {index: 1, title: "My Senior Project!", img:"projImg.png", git_hub: "", proj_link: "", description: "description text", isVisible: false},
+    {index: 2, title: "Project 3", img:"projImg.png", git_hub: "", proj_link: "", description: "description text", isVisible: false}
+  ]
+
+  displayProjects = [
+    this.projects[0],
+    this.projects[1],
+    this.projects[2]
+  ]
+
+  toggleBox(project: any) {
+    this.isBoxDrawn = !this.isBoxDrawn;
+  }
+}
