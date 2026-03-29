5c2634859e6f35fcba9667ea2e3baa7a32e4a8fd
Jimmy Ren <jimmyren@Jimmys-MacBook-Pro.local>
2026-03-28 20:12:14 -0400

    finished experience component

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './experience.component.html',
 })
-export class ExperienceComponent {}
+export class ExperienceComponent {
+    experience = [
+    {index: 0, title: "IT Tech", company: "T-Mobile", duration: "", description: ""},
+    {index: 1, title: "Teaching Assistant", company: "Temple", duration: "", description: ""},
+    {index: 2, title: "Computer Engineer", company: "GreenXT", duration: "", description: ""}
+  ]
+
+  displayExp = [
+
+  ]
+
+
+}
