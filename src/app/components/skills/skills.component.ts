a2cae3b8d3c6b96db6d84360498ce02f760f4b76
Jimmy Ren <jimmyren@Jimmys-MacBook-Pro.local>
2026-04-02 09:02:33 -0400

    updates experience and skills

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -14,11 +14,11 @@ export class SkillsComponent {
 
   /* ADD SKILLS HERE */
   skills = [
-    { index: 0, img: "back-end-img.png", title: "Backend Stack", description: "•Java v26 \n •Python v3.14 \n •C v23 \n •C++ v23 \n •C# v14"},
-    { index: 1, img: "front-end-img.png", title: "Frontend Stack", description: "•Javascript / HTML5 / CSS \n •Tailwind CSS v4 \n •Typescript v6"},
-    { index: 2, img: "frameworks-img.png", title: "Frameworks", description: "•Angular v21\n •React v19 \n •Spring Boot v4"},
-    { index: 3, img: "tools-img.png", title: "Tools", description: "•VS Code \n •AWS \n •IntelliJ \n •Github \n •Jupyter Notebooks"},
-    { index: 4, img: "llm-img.png", title: "LLMs", description: "•ChatGPT 5.3 \n •Gemini 1.5 Flash \n •Claude 3 Sonnet \n •DeepSeek-V3"},
+    { index: 0, img: "back-end-img.png", title: "Backend Stack", description: "• Java v26 \n • Python v3.14 \n • C v23 \n • C++ v23 \n •C # v14"},
+    { index: 1, img: "front-end-img.png", title: "Frontend Stack", description: "• Javascript / HTML5 / CSS \n • Tailwind CSS v4 \n • Typescript v6"},
+    { index: 2, img: "frameworks-img.png", title: "Frameworks", description: "• Angular v21\n • React v19 \n • Spring Boot v4"},
+    { index: 3, img: "tools-img.png", title: "Tools", description: "• VS Code \n • AWS \n • IntelliJ \n • Github \n • Jupyter Notebooks"},
+    { index: 4, img: "llm-img.png", title: "LLMs", description: "• ChatGPT 5.3 \n • Gemini 1.5 Flash \n • Claude 3 Sonnet \n • DeepSeek-V3"},
   ];
 
   displaySkills = [
